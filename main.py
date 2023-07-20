532db02a34d5f97b216c5d4769a7b54196ca77d8
June <andrealee97@naver.com>
2023-07-20 16:50:21 +0900

    스태시 후 커밋

--- main.py
+++ main.py
@@ -30,5 +30,5 @@ print(num1,'x',num2,'=',res)
 res = div_func(num1, num2)
 print(num1,'/',num2,'=',res)
 
-res = sq_func(num1, num2)
-print(num1,'x',num2,'=',res)
+res = sq_func(num1)
+print(num1,'x',num1,'=',res)
